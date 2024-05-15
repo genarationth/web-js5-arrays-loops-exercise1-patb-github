cc23e6087e254d9953a64f573d11ae518640e80d
patb-github <165882561+patb-github@users.noreply.github.com>
2024-05-15 13:14:57 +0700

    Create ex1.js

---
+++ ex1.js
@@ -0,0 +1,11 @@
+// 1.1
+let myAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G']; // myAlphabet has a length of 8; 
+const myAlphabetLength = () => console.log(myAlphabet.length);
+
+// 1.2
+const planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter"];
+for (let i = 0; i < planets.length; i++) console.log(`${i}: ${planets[i]}`);
+
+// 1.3
+let myArr = [ 1, 2, 'One', true];
+for (let i = 0; i < myArr.length; i++) console.log(myArr[i]);
